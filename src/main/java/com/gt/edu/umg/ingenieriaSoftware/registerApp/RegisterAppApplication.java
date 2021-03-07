fe24121b19c98092e374a7f74f9348acb91e2886
oscarrl98 <oscar_rlp@hotmail.es>
2021-03-07 17:35:09 -0600

    Codigo del problema de RegisterApp

---
+++ src/main/java/com/gt/edu/umg/ingenieriaSoftware/registerApp/RegisterAppApplication.java
@@ -0,0 +1,13 @@
+package com.gt.edu.umg.ingenieriaSoftware.registerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gt/edu/umg/ingenieriaSoftware/registerApp/RegisterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gt.edu.umg.ingenieriaSoftware.registerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
