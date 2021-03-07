a0b9dbe2d91809dd03f7d5a3d9912909b1713ad3
oscarrl98 <oscar_rlp@hotmail.es>
2021-03-07 17:21:36 -0600

    Codigo del problema en movieApp

---
+++ src/main/java/com/gt/edu/umg/ingenieriaSoftware/movieApp/IUserAcountsActions.java
@@ -0,0 +1,20 @@
+package com.gt.edu.umg.ingenieriaSoftware.movieApp;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public interface IUserAcountsActions {
+
+
+    public void PlayMovieFull() throws Exception;
+
+    public void PlayMovieFree();
+
+    public void DownloadMovie() throws Exception;
+
+    public void TakeScreenshot() throws Exception;
+    public void SkipAd() throws Exception;
+
+}
